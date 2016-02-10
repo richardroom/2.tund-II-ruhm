fa1fbb6c4009ca964ea59134ecf07143b290a6e9
richardroom <richard.roomusaar@gmail.com>
2016-02-10 11:14:59 +0200

    MouseEvent

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-  (function(){
-    "use strict";
+(function(){
+  "use strict";
 
 
-      var Moosipurk = function(){
+  var Moosipurk = function(){
 
-        //singelton pattern
-        if(Moosipurk.instance){
-          return Moosipurk.instance;
-        }
-        Moosipurk.instance = this; //this=Moosipurk
+    // SINGLETON PATTERN (4 rida)
+    if(Moosipurk.instance){
+      return Moosipurk.instance;
+    }
+    Moosipurk.instance = this; // this viitab moosipurgile
 
-        console.log('moosipurgi sees');
+    console.log(this);
+    //console.log('moosipurgi sees');
 
-      };
+    //panen rakenduse tööle
+    this.init();
+  };
 
-      window.onload = function(){
-        var app = new Moosipurk();
-        var app2 = new Moosipurk();
+  //kõik moosipurgi funktsioonid tulevad siia sisse
+  Moosipurk.prototype = {
+    init: function(){
+      console.log('rakendus käivitus');
+      // Siia tuleb esialgne loogika
+      // hakka kuulama hiireklõpse
+      this.bindMouseEvents();
+    },
+    bindMouseEvents: function(){
+      document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+    },
+    addNewClick: function(event){
+      console.log(event);
 
-      };
+    }
+  };
 
-  }) ();
+
+  window.onload = function(){
+    var app = new Moosipurk();
+  };
+
+})();
